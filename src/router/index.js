630066a6fab6739e2df1d1f6fbe14255e62edc04
Dragos Sasaran <dragos.fortech@fortech.ro>
2022-04-27 17:38:26 +0300

    base-app-with-login-logout

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/HomeView.vue';
+import Login from '../views/LoginView.vue';
+import Register from '../views/RegisterView.vue';
+import Create from '../views/CreateView.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+  },
+  {
+    path: '/create',
+    name: 'Create',
+    component: Create,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+// Change document titles
+
+// Route guard for auth routes
+
+export default router;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: 'home-view',
+  components: {},
+  setup() {
+    // Create data / vars
+
+    // Get data
+
+    // Run data function
+
+    return {};
+  },
+};
 </script>
